0f389681656d06d6d784d5bd42dec6ad02cee614
Alberto Benatti <albi.benni8@gmail.com>
2025-08-14 18:10:22 +0200

    wip

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +11,27 @@ func main() {
 	file, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal("error", err)
+	}
+	str := ""
+	for {
+		data := make([]byte, 8)
+		n, err := file.Read(data)
+		if err != nil {
+			break
+		}
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != 0 {
+			str += string(data[:i])
+			data = data[i+1:]
 
+			fmt.Printf("read: %s\n", str)
+			str = ""
+
+		}
+		str += string(data)
+
+	}
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
 	}
 }
