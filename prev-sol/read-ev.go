1f36b7f2a434a2e5bab3b2a95fbbe8605d18aec2
Alberto Benatti <albi.benni8@gmail.com>
2025-08-15 14:52:20 +0200

    save read

---
+++ prev-sol/read-ev.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+
+				// fmt.Printf("read: %s\n", str)
+				out <- str
+				str = ""
+
+			}
+			str += string(data)
+
+		}
+		if len(str) != 0 {
+			// fmt.Printf("read: %s\n", str)
+			out <- str
+		}
+	}()
+
+	return out
+}
+
+func main() {
+	file, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal("error", err)
+	}
+
+	defer file.Close()
+
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
